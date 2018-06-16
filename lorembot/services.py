328dcfc494ecc40c8daa5663b32be13a392cd02f
Thiago Pereira <thiagoflames@gmail.com>
2018-06-16 10:41:13 -0300

    Fetch updates from telegram
    Signed-off-by: Thiago Pereira <thiagoflames@gmail.com>
    Co-authored-by: Breno Mariz <mrzbeo@gmail.com>
    Co-authored-by: Igor Gabriel <igorgabriel515@gmail.com>
    Co-authored-by: Indiara Duarte <indiara96d@gmail.com>
    Co-authored-by: Kairon Velozo <kairon333@gmail.com>

--- lorembot/services.py
+++ lorembot/services.py
@@ -22,6 +22,35 @@ class SubjectService(object):
     """
     Notify all observers.
     """
-    def notify(self):
+    def notify(self, update):
         for observer in self._observers
-            observer.update(self)
+            observer.update(update)
+
+class SubjectTelegram(SubjectService):
+
+    _ENDPOINT = "https://api.telegram.org/"
+    _STOP_FETCHING = False
+
+    def __init__(self):
+        # Read config file to get token
+        self._TOKEN = ""  # get token from file
+        self.BOT_ENDPOINT = "{}/{}".format(self._ENDPOINT, self._TOKEN)
+
+    """
+    Fetches updates from telegram api.
+    """
+    def fetch_updates():
+        updates_url = "{}/getUpdates?timeout=100".format(self.BOT_ENDPOINT)
+        while True:
+            if self._STOP_FETCHING:
+                return
+
+            updates = []  # Actually make the request
+            for update in updates:
+                self.notify(update)
+
+    """
+    Stops a subject from fecthing updates from telegram api.
+    """
+    def stop_fetching():
+        self._STOP_FETCHING = True
