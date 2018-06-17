b558867ab09f25af2bf45698e93463304152ac6a
Kairon Velozo <kairon333@gmail.com>
2018-06-17 12:26:47 -0300

    Making setup install requirements on package installation. Co-authored-by: Breno Mariz <mrzbeo@gmail.com> Co-authored-by: Igor Gabriel <igorgabriel515@gmail.com> Co-authored-by: Indiara Duarte <indiara96d@gmail.com> Co-authored-by: Thiago Ribeiro <thiagoflames@gmail.com>

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+ install_requires = [
+    'requests==2.19.1',
+    'SQLAlchemy==1.2.8'
+]
+
 setuptools.setup(
     name="lorem-bot",
     version="0.0.1",
@@ -13,7 +18,9 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/DSW12018/LoremBot",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=["tests"]),
+    include_package_data=True,
+    install_requires=install_requires,
     classifiers=(
         "License :: OSI Approved :: MIT License",
         'Programming Language :: Python :: 3',
@@ -22,4 +29,3 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ),
 )
-
