1bcde65398de2967d17b6d60bc173c37e73bb25a
Kairon Velozo <kairon333@gmail.com>
2018-06-18 05:43:47 -0300

    Passing message as parameter to command handler.
    Signed-off-by: Kairon Velozo <kairon333@gmail.com>
    Co-authored-by: Breno Mariz <mrzbeo@gmail.com>
    Co-authored-by: Igor Gabriel <igorgabriel515@gmail.com>
    Co-authored-by: Indiara Duarte <indiara96d@gmail.com>
    Co-authored-by: Thiago Ribeiro <thiagoflames@gmail.com>

--- lorembot/command_handler.py
+++ lorembot/command_handler.py
@@ -2,7 +2,7 @@ from .config import config
 
 
 class CommandHandler():
-    def match(cmd):
+    def match(cmd, message):
         for command in config.commands:
             if cmd == command.command:
-                command.function()
+                command.function(message)
