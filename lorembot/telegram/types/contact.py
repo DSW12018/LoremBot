87c5050bb82a3c3139f97fb05c37493df97b97d4
Igor Gabriel <igorgabriel515@gmail.com>
2018-06-18 00:25:51 -0300

    Creating contact class.
    Signed-off-by: Igor Gabriel <igorgabriel515@gmail.com>
    Co-authored-by: Breno Mariz <mrzbeo@gmail.com>
    Co-authored-by: Thiago Ribeiro <thiagoflames@gmail.com>
    Co-authored-by: Indiara Duarte <indiara96d@gmail.com>
    Co-authored-by: Kairon Velozo <kairon333@gmail.com>

--- lorembot/telegram/types/contact.py
+++ lorembot/telegram/types/contact.py
@@ -1,12 +1,13 @@
-import json
 from .base import Base
 
 class Contact(Base):
 
-    def __init__(self, *args, **kwargs):
-        # required
-        self.phone_number = kwargs['phone_number']
-        self.first_name = kwargs['first_name']
-        # optional
-        self.last_name = kwargs['last_name']
-        self.user_id = kwargs['user_id']
+    REQUIRED = [
+        'phone_number',
+        'first_name'
+    ]
+
+    OPTIONAL = [
+        'last_name',
+        'user_id'
+    ]
