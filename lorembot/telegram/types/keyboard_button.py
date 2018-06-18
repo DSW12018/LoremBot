a1b57dc81c28ad7202e03025fe6aec394c229704
Igor Gabriel <igorgabriel515@gmail.com>
2018-06-18 00:49:12 -0300

    Modifying KeyboardButton class.
    Signed-off-by: Igor Gabriel <igorgabriel515@gmail.com>
    Co-authored-by: Breno Mariz <mrzbeo@gmail.com>
    Co-authored-by: Thiago Ribeiro <thiagoflames@gmail.com>
    Co-authored-by: Indiara Duarte <indiara96d@gmail.com>
    Co-authored-by: Kairon Velozo <kairon333@gmail.com>

--- lorembot/telegram/types/keyboard_button.py
+++ lorembot/telegram/types/keyboard_button.py
@@ -3,7 +3,11 @@ from .base import Base
 
 class KeyboardButton(Base):
 
-    def __init__(self, **kwargs):
-        self.text = kwargs['text']
-        self.request_contact = kwargs['request_contact']
-        self.request_location = kwargs['request_location']
+    REQUIRED = [
+        'text'
+    ]
+
+    OPTIONAL = [
+        'request_contact',
+        'request_location'
+    ]
