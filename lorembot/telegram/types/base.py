9e8049064893422b4b8edc10e23bbd6e11b56297
Igor Gabriel <igorgabriel515@gmail.com>
2018-06-18 00:30:46 -0300

    Creating Document class.
    Signed-off-by: Igor Gabriel <igorgabriel515@gmail.com>
    Co-authored-by: Breno Mariz <mrzbeo@gmail.com>
    Co-authored-by: Thiago Ribeiro <thiagoflames@gmail.com>
    Co-authored-by: Indiara Duarte <indiara96d@gmail.com>
    Co-authored-by: Kairon Velozo <kairon333@gmail.com>

--- lorembot/telegram/types/base.py
+++ lorembot/telegram/types/base.py
@@ -6,11 +6,3 @@ class Base(Object):
     @property
     def as_json(self):
         return json.dumps(self.__dict__)
-import json
-from ..object import Object
-
-class TypeBase(Object):
-
-    @property
-    def as_json(self):
-        return json.dumps(self.__dict__)
