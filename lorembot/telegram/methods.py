1b49d3f8e4c942c7abf804026b9e29b4517572f0
Indiara <indiara96d@gmail.com>
2018-06-17 22:55:10 -0400

    Adding Forward Message Co-authored-by: Breno Mariz <mrzbeo@gmail.com> Co-authored-by: Igor Gabriel <igorgabriel515@gmail.com> Co-authored-by: Kairon Velozo <kairon333@gmail.com> Co-authored-by: Thiago Ribeiro <thiagoflames@gmail.com>

--- lorembot/telegram/methods.py
+++ lorembot/telegram/methods.py
@@ -4,6 +4,8 @@ from lorembot.config import uri
 
 class BaseMethod(object):
 
+    RESTRICTED = []
+
     def __init__(self, *args, **kwargs):
         # required
         self.build_required(kwargs)
@@ -15,17 +17,17 @@ class BaseMethod(object):
         dict = {}
         for required in REQUIRED and not required in RESTRICTED:
             dict[required] = self[required]
-        
+
         for optional in OPTIONAL
             if not self[optional] is None and not optional in RESTRICTED:
                 dict[optional] = self[optional]
 
         for restricted in RESTRICTED:
             if not self[restricted] is None:
-                dict[restricted] = self[restricted].as_json    
-        
+                dict[restricted] = self[restricted].as_json
+
         return json.dumps(dict)
-    
+
     def build_required(self, *args, **kwargs):
         for required in REQUIRED:
             self[required] = kwargs[required]
@@ -42,13 +44,13 @@ class BaseMethod(object):
         requests.post(url, self.as_json)
 
 class SendMessage(BaseMethod):
-    
+
     ENDPOINT = '/sendMessage'
-    
+
     RESTRICTED = [
         'reply_markup'
     ]
-    
+
     REQUIRED = [
         'chat_id',
         'text'
@@ -62,3 +64,17 @@ class SendMessage(BaseMethod):
         'reply_to_message_id',
         'reply_markup'
     ]
+
+class ForwardMessage(BaseMethod):
+
+    ENDPOINT = '/forwardMessage'
+
+    REQUIRED = [
+        'chat_id',
+        'from_chat_id',
+        'message_id'
+    ]
+
+    OPTIONAL = [
+        'disable_notification'
+   ]
